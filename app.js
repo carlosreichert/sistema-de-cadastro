54e47300f405e5ff3962609a666210fed6704015
Carlos Henrique Colombo Reichert <carlos.reichert2@gmail.com>
2023-12-05 19:47:50 -0300

    adicionando mais um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectado ao banco de dados")
 // mais um console log para teste
 console.log("criando console para teste")
+console.log("Enviando para o GitHub")
